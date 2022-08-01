2009710b0fd524780f68443a15a8c018aa98baad
Someshkatle <someshkatle2970@gmail.com>
2022-08-01 12:52:14 +0530

    page load

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import '../styles/App.css';
 import Welcome from './Welcome';
+import { useState } from "react";
 
 const App = () => {
+
+  const [name, setName] = useState("")
+
+  function getData(val)
+  {
+    setName(val.target.value)
+  }
+
   return (
-    <Welcome name="" />
+    <>
+    <Welcome name={name} />
+    <input type="text" value={name} placeholder="Enter Your Name" onChange={getData}/>
+    
+    </>
   )
 }
 
